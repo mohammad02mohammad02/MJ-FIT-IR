ad64386c374c0fb87e187a01fc14531dcb45ca91
mohammad jalali <jlalyjlaly423@gmail.com>
2026-07-12 02:04:35 +0330

    Create Instagram.tsx

---
+++ app/app/componencomponents/app/components/public/images/public/public/public/app/components/components/lib/app/app/components/components/components/components/components/components/hooks/lib/app/components/components/components/components/components/components/components/components/Instagram.tsx
@@ -0,0 +1,27 @@
+export default function Instagram() {
+  return (
+    <section className="py-28">
+
+      <div className="container text-center">
+
+        <p className="gold uppercase tracking-[6px]">
+          Follow Me
+        </p>
+
+        <h2 className="title mt-5">
+          @MJ.FIT.IR
+        </h2>
+
+        <a
+          href="https://instagram.com/mj.fit.ir"
+          className="btn mt-10"
+          target="_blank"
+        >
+          Instagram
+        </a>
+
+      </div>
+
+    </section>
+  );
+}
