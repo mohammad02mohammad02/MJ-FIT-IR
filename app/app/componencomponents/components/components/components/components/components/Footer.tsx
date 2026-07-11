bae3997aa67a4f2bda3a2cc751e7dffd59213656
mohammad jalali <jlalyjlaly423@gmail.com>
2026-07-12 01:39:45 +0330

    Create Footer.tsx

---
+++ app/app/componencomponents/components/components/components/components/components/Footer.tsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <footer className="py-10 border-t border-white/10">
+
+      <div className="container flex flex-col lg:flex-row items-center justify-between gap-6">
+
+        <h2 className="text-4xl font-black gold tracking-[8px]">
+          MJ
+        </h2>
+
+        <p className="text-white/60 text-center">
+          © 2025 MJ.FIT.IR — Mohammad Jalali
+        </p>
+
+        <p className="gold">
+          Engineer Your Body.
+        </p>
+
+      </div>
+
+    </footer>
+  );
+}
