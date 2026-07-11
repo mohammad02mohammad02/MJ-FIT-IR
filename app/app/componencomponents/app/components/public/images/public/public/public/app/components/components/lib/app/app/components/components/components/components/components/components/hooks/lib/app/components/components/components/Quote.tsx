4886546e9af644c37b9b24f2f8ce30a2d5f36f1c
mohammad jalali <jlalyjlaly423@gmail.com>
2026-07-12 02:00:49 +0330

    Create Quote.tsx

---
+++ app/app/componencomponents/app/components/public/images/public/public/public/app/components/components/lib/app/app/components/components/components/components/components/components/hooks/lib/app/components/components/components/Quote.tsx
@@ -0,0 +1,19 @@
+export default function Quote() {
+  return (
+    <section className="py-32">
+
+      <div className="container text-center">
+
+        <h2 className="text-5xl font-black leading-tight max-w-5xl mx-auto">
+
+          "Discipline Beats
+          Motivation Every
+          Single Time."
+
+        </h2>
+
+      </div>
+
+    </section>
+  );
+}
