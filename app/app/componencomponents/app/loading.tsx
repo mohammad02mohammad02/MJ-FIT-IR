6e8a3d055ccb7f907303352d30e8330aa53bbb67
mohammad jalali <jlalyjlaly423@gmail.com>
2026-07-12 01:43:36 +0330

    Create loading.tsx

---
+++ app/app/componencomponents/app/loading.tsx
@@ -0,0 +1,19 @@
+export default function Loading() {
+  return (
+    <main className="w-full h-screen bg-black flex items-center justify-center">
+
+      <div className="flex flex-col items-center">
+
+        <h1 className="text-7xl font-black tracking-[10px] text-[#D4AF37] animate-pulse">
+          MJ
+        </h1>
+
+        <p className="mt-6 text-white/70 tracking-[8px] uppercase">
+          Loading...
+        </p>
+
+      </div>
+
+    </main>
+  );
+}
