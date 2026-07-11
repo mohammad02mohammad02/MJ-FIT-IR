8fc0f3da21873c0ee3cf50d1b0a8c8b032738a0f
mohammad jalali <jlalyjlaly423@gmail.com>
2026-07-12 01:57:22 +0330

    Create constants.ts

---
+++ app/app/componencomponents/app/components/public/images/public/public/public/app/components/components/lib/app/app/components/components/components/components/components/components/hooks/lib/constants.ts
@@ -0,0 +1,16 @@
+export const SITE = {
+  name: "MJ.FIT.IR",
+
+  owner: "Mohammad Jalali",
+
+  instagram: "https://instagram.com/mj.fit.ir",
+
+  phone: "09931693826",
+
+  slogan: "Engineer Your Body.",
+
+  description:
+    "Elite Fitness Coach",
+
+  email: "",
+};
