2b9f8fcecac3a882345a4b13718dc6c4872ab6f4
mohammad jalali <jlalyjlaly423@gmail.com>
2026-07-12 02:10:33 +0330

    Create LoaderLine.tsx

---
+++ app/app/componencomponents/app/components/public/images/public/public/public/app/components/components/lib/app/app/components/components/components/components/components/components/hooks/lib/app/components/components/components/components/components/components/components/components/public/videos/components/components/components/components/public/components/components/LoaderLine.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function LoaderLine() {
+  return (
+    <div className="fixed top-0 left-0 w-full h-[3px] overflow-hidden z-[9999]">
+
+      <div className="w-1/2 h-full bg-[#D4AF37] animate-pulse" />
+
+    </div>
+  );
+}
