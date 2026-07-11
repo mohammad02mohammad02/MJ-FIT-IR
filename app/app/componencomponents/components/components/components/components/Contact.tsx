8bbe544112cd590abeda65ea568e465f29b914ba
mohammad jalali <jlalyjlaly423@gmail.com>
2026-07-12 01:39:09 +0330

    Create Contact.tsx

---
+++ app/app/componencomponents/components/components/components/components/Contact.tsx
@@ -0,0 +1,55 @@
+export default function Contact() {
+  return (
+    <section id="contact">
+
+      <div className="container">
+
+        <div className="glass rounded-[40px] p-16 text-center">
+
+          <p className="gold uppercase tracking-[6px]">
+            CONTACT
+          </p>
+
+          <h2 className="title mt-5 mb-8">
+            Start Your Transformation
+          </h2>
+
+          <p className="subtitle mb-12">
+            Contact me to receive your personalized
+            workout and nutrition program.
+          </p>
+
+          <div className="space-y-5 text-xl">
+
+            <p>
+              📞 09931693826
+            </p>
+
+            <p>
+              📷 Instagram :
+              <a
+                href="https://instagram.com/mj.fit.ir"
+                target="_blank"
+                className="gold ml-2"
+              >
+                @MJ.FIT.IR
+              </a>
+            </p>
+
+          </div>
+
+          <a
+            href="https://instagram.com/mj.fit.ir"
+            target="_blank"
+            className="btn mt-12"
+          >
+            CONTACT NOW
+          </a>
+
+        </div>
+
+      </div>
+
+    </section>
+  );
+}
