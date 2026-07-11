dd6bfd548f959b796919d4d8703c3012bf934544
mohammad jalali <jlalyjlaly423@gmail.com>
2026-07-12 02:00:11 +0330

    Create Divider.tsx

---
+++ app/app/componencomponents/app/components/public/images/public/public/public/app/components/components/lib/app/app/components/components/components/components/components/components/hooks/lib/app/components/components/Divider.tsx
@@ -0,0 +1,9 @@
+export default function Divider() {
+  return (
+    <div className="container py-8">
+
+      <div className="w-full h-px bg-gradient-to-r from-transparent via-[#D4AF37] to-transparent" />
+
+    </div>
+  );
+}
