423ef8263d1bf06f9c796636dbafeec21baf5818
mohammad jalali <jlalyjlaly423@gmail.com>
2026-07-12 02:10:00 +0330

    Create SEO.tsx

---
+++ app/app/componencomponents/app/components/public/images/public/public/public/app/components/components/lib/app/app/components/components/components/components/components/components/hooks/lib/app/components/components/components/components/components/components/components/components/public/videos/components/components/components/components/public/components/SEO.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MJ.FIT.IR | Mohammad Jalali",
+  description:
+    "Official website of Mohammad Jalali | Elite Fitness Coach",
+  keywords: [
+    "MJ FIT",
+    "Mohammad Jalali",
+    "Fitness Coach",
+    "Workout",
+    "Nutrition",
+    "Gym",
+  ],
+  openGraph: {
+    title: "MJ.FIT.IR",
+    description: "Elite Fitness Coach",
+    url: "https://mj.fit.ir",
+    siteName: "MJ.FIT.IR",
+    locale: "en_US",
+    type: "website",
+  },
+};
