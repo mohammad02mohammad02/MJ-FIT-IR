70e62f221926009f2d50519befacc0882fa1670c
mohammad jalali <jlalyjlaly423@gmail.com>
2026-07-12 01:49:48 +0330

    Create index.ts

---
+++ app/app/componencomponents/app/components/public/images/public/public/public/app/components/components/index.ts
@@ -0,0 +1,9 @@
+export { default as Navbar } from "./Navbar";
+export { default as Hero } from "./Hero";
+export { default as About } from "./About";
+export { default as Services } from "./Services";
+export { default as Gallery } from "./Gallery";
+export { default as Contact } from "./Contact";
+export { default as Footer } from "./Footer";
+export { default as Button } from "./Button";
+export { default as ScrollTop } from "./ScrollTop";
